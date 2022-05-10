f83e79b56612ee029300d48f1c5e28b02dea6d12
nicolay korobeinikov <kolyan125125@yandex.ru>
2022-05-10 23:11:35 +0300

    solution

--- shared-ptr.h
+++ shared-ptr.h
@@ -1,29 +1,114 @@
 #pragma once
 
 #include <cstddef>
+#include <memory>
 
 struct ControlBlock {
   size_t weak_ptr_cnt;
   size_t shared_ptr_cnt;
+
+  ControlBlock(size_t weak_ptr_cnt, size_t shared_ptr_cnt)
+      : weak_ptr_cnt(weak_ptr_cnt), shared_ptr_cnt(shared_ptr_cnt) {}
+
+  void add_shared() {
+    weak_ptr_cnt++;
+    shared_ptr_cnt++;
+  }
+
+  void remove_shared() {
+    weak_ptr_cnt--;
+    shared_ptr_cnt--;
+  }
+
+  void add_weak() {
+    weak_ptr_cnt++;
+  }
+
+  void remove_weak() {
+    weak_ptr_cnt--;
+  }
+
+  virtual ~ControlBlock() {}
+
+  virtual void deleteObjectPtr() = 0;
+};
+
+template <class T, class Deleter>
+struct ControlBlockWithPointer : ControlBlock {
+  T* object_ptr;
+  Deleter deleter;
+  ControlBlockWithPointer(T* object_ptr, Deleter deleter)
+      : ControlBlock(1, 1), object_ptr(object_ptr), deleter(deleter) {}
+
+  void deleteObjectPtr() override {
+    this->deleter(object_ptr);
+    object_ptr = nullptr;
+  }
+};
+
+template <class T>
+struct ControlBlockWithValue : ControlBlock {
+  std::aligned_storage_t<sizeof(T), alignof(T)> object_storage;
+
+  template <class... Args>
+  ControlBlockWithValue(Args... args)
+      : ControlBlock(1, 1) {
+    ::new (&object_storage) T(std::forward<Args>(args)...);
+  }
+
+  void deleteObjectPtr() override {
+    reinterpret_cast<T*>(&object_storage)->~T();
+  }
+
+  ~ControlBlockWithValue() {
+
+  }
 };
 
 template <typename T>
 class weak_ptr;
+
+template<typename T>
+class shared_ptr;
 
 template <typename T>
 class shared_ptr {
 public:
   friend class weak_ptr<T>;
 
+  template <typename V, typename... Args>
+  friend shared_ptr<V> make_shared(Args&&... args);
+
+  template <typename Y>
+  friend class shared_ptr;
+
   shared_ptr() noexcept = default;
 
-  shared_ptr(nullptr_t) noexcept {};
+  explicit shared_ptr(std::nullptr_t) noexcept {}
 
-  shared_ptr(T* ptr_)
-      : object_ptr(ptr_), control_block_ptr(new ControlBlock{1, 1}) {}
+  template<typename V, typename  Deleter = std::default_delete<V>>
+  explicit shared_ptr(V* ptr_, Deleter deleter = std::default_delete<V>())
+      : object_ptr(ptr_),
+        control_block_ptr(new ControlBlockWithPointer{ptr_, deleter}) {}
 
-  shared_ptr(const shared_ptr& other) noexcept {
-    *this = other;
+  template<typename V>
+  shared_ptr(const shared_ptr<V>& other, T* object_ptr) : object_ptr(object_ptr), control_block_ptr(other.control_block_ptr) {
+    if (control_block_ptr != nullptr) {
+      control_block_ptr->add_shared();
+    }
+  }
+
+  shared_ptr(const shared_ptr& other) noexcept : object_ptr(other.object_ptr), control_block_ptr(other.control_block_ptr) {
+    if (control_block_ptr != nullptr) {
+      control_block_ptr->add_shared();
+    }
+  }
+
+  template<class V>
+  shared_ptr(const shared_ptr<V>& other) noexcept : object_ptr(other.object_ptr), control_block_ptr(other.control_block_ptr) {
+    if (control_block_ptr != nullptr) {
+      control_block_ptr->add_shared();
+    }
   }
 
   shared_ptr(shared_ptr&& other) noexcept
@@ -40,9 +125,8 @@ public:
     this->~shared_ptr();
     this->object_ptr = other.object_ptr;
     this->control_block_ptr = other.control_block_ptr;
-    if (control_block_ptr != nullptr) {
-      control_block_ptr->weak_ptr_cnt++;
-      control_block_ptr->shared_ptr_cnt++;
+    if (control_block_ptr) {
+      control_block_ptr->add_shared();
     }
     return *this;
   }
@@ -61,8 +145,16 @@ public:
     return object_ptr;
   }
 
+  operator T*() const noexcept {
+    return object_ptr;
+  }
+
+  bool operator==(const shared_ptr& rhs) {
+    return rhs.control_block_ptr == control_block_ptr;
+  }
+
   operator bool() const noexcept {
-    return object_ptr != nullptr;
+    return object_ptr;
   }
 
   T& operator*() const noexcept {
@@ -74,7 +166,7 @@ public:
   }
 
   std::size_t use_count() const noexcept {
-    if (control_block_ptr == nullptr) {
+    if (!control_block_ptr) {
       return 0;
     }
     return control_block_ptr->shared_ptr_cnt;
@@ -84,18 +176,18 @@ public:
     this->~shared_ptr();
   }
 
-  void reset(T* new_ptr) {
+  template<class V, class Deleter = std::default_delete<V>>
+  void reset(V* new_ptr, Deleter deleter = std::default_delete<V>()) {
     reset();
     object_ptr = new_ptr;
-    control_block_ptr = new ControlBlock{1, 1};
+    control_block_ptr = new ControlBlockWithPointer{new_ptr, deleter};
   }
 
   ~shared_ptr() {
-    if (control_block_ptr != nullptr) {
-      control_block_ptr->shared_ptr_cnt--;
-      control_block_ptr->weak_ptr_cnt--;
+    if (control_block_ptr) {
+      control_block_ptr->remove_shared();
       if (control_block_ptr->shared_ptr_cnt == 0) {
-        delete object_ptr;
+        control_block_ptr->deleteObjectPtr();
       }
       if (control_block_ptr->weak_ptr_cnt == 0) {
         delete control_block_ptr;
@@ -117,10 +209,40 @@ public:
 
   weak_ptr(const shared_ptr<T>& other) noexcept
       : control_block_ptr(other.control_block_ptr), object_ptr(other.get()) {
-    if (other.control_block_ptr != nullptr) {
-      other.control_block_ptr->weak_ptr_cnt++;
+    if (other.control_block_ptr) {
+      other.control_block_ptr->add_weak();
     }
   }
+
+  weak_ptr(const weak_ptr<T>& other) noexcept : control_block_ptr(other.control_block_ptr), object_ptr(other.object_ptr) {
+    if (other.control_block_ptr) {
+      other.control_block_ptr->add_weak();
+    }
+  }
+
+  weak_ptr& operator=(const weak_ptr<T>& other) noexcept {
+    if (control_block_ptr == other.control_block_ptr) {
+      return *this;
+    }
+    this->~weak_ptr();
+    control_block_ptr = other.control_block_ptr;
+    object_ptr = other.object_ptr;
+    if (control_block_ptr) {
+      control_block_ptr->add_weak();
+    }
+    return *this;
+  }
+
+  weak_ptr& operator=(weak_ptr<T>&& other) noexcept {
+    if (control_block_ptr == other.control_block_ptr) {
+      return *this;
+    }
+    this->~weak_ptr();
+    std::swap(this->control_block_ptr, other.control_block_ptr);
+    std::swap(this->object_ptr, other.object_ptr);
+    return *this;
+  }
+
   weak_ptr& operator=(const shared_ptr<T>& other) noexcept {
     if (control_block_ptr == other.control_block_ptr) {
       return *this;
@@ -128,28 +250,27 @@ public:
     this->~weak_ptr();
     control_block_ptr = other.control_block_ptr;
     object_ptr = other.object_ptr;
-    if (control_block_ptr != nullptr) {
-      control_block_ptr->weak_ptr_cnt++;
+    if (control_block_ptr) {
+      control_block_ptr->add_weak();
     }
     return *this;
   }
 
   shared_ptr<T> lock() const noexcept {
-    if (control_block_ptr == nullptr ||
+    if (!control_block_ptr ||
         control_block_ptr->shared_ptr_cnt == 0) {
       return shared_ptr<T>(nullptr);
     }
     auto shared = shared_ptr<T>(nullptr);
     shared.object_ptr = object_ptr;
     shared.control_block_ptr = control_block_ptr;
-    control_block_ptr->shared_ptr_cnt++;
-    control_block_ptr->weak_ptr_cnt++;
+    control_block_ptr->add_shared();
     return shared;
   }
 
   ~weak_ptr() {
-    if (control_block_ptr != nullptr) {
-      control_block_ptr->weak_ptr_cnt--;
+    if (control_block_ptr) {
+      control_block_ptr->remove_weak();
       if (control_block_ptr->weak_ptr_cnt == 0) {
         delete control_block_ptr;
       }
@@ -159,11 +280,15 @@ public:
   }
 
 private:
-  ControlBlock* control_block_ptr = nullptr;
+  ControlBlock * control_block_ptr = nullptr;
   T* object_ptr = nullptr;
 };
 
 template <typename T, typename... Args>
 shared_ptr<T> make_shared(Args&&... args) {
-  return shared_ptr<T>(new T(std::forward<Args>(args)...));
+  auto controlBlock = new ControlBlockWithValue<T>(args...);
+  auto shared = shared_ptr<T>(nullptr);
+  shared.control_block_ptr = controlBlock;
+  shared.object_ptr = reinterpret_cast<T*>(&controlBlock->object_storage);
+  return shared;
 }
